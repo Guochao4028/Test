c89ccafc3b5538835c99f3181c130cbe1ad37b7d
guochao <Thirteen@WangShuhaodeMBP.lan>
2017-02-10 16:43:25 +0800

    通讯录转json 完成

--- AddressBookToJson/AddressBookToJson/ViewController.m
+++ AddressBookToJson/AddressBookToJson/ViewController.m
@@ -30,7 +30,7 @@
 
 @interface ViewController (){
 
-    ABAddressBookRef *_addressBookRef;
+    ABAddressBookRef addressBookRef;
 }
 
 - (IBAction)addressBookToJsonAction:(id)sender;
@@ -77,17 +77,17 @@
     //这个变量用于记录授权是否成功，即用户是否允许我们访问通讯录
     BOOL __block tip = NO;
     //声明一个通讯簿的引用
-    ABAddressBookRef addBook =nil;
+    addressBookRef =nil;
     //因为在IOS6.0之后和之前的权限申请方式有所差别，这里做个判断
     if ([[UIDevice currentDevice].systemVersion floatValue]>=6.0) {
         //创建通讯簿的引用
-        addBook=ABAddressBookCreateWithOptions(NULL, NULL);
+        addressBookRef=ABAddressBookCreateWithOptions(NULL, NULL);
         //创建一个出事信号量为0的信号
         dispatch_semaphore_t sema=dispatch_semaphore_create(0);
         //申请访问权限
-        ABAddressBookRequestAccessWithCompletion(addBook, ^(bool greanted, CFErrorRef error)        {
+        ABAddressBookRequestAccessWithCompletion(addressBookRef, ^(bool greanted, CFErrorRef error)        {
             //greanted为YES是表示用户允许，否则为不允许
-            if (!greanted) {
+            if (greanted) {
                 tip = YES;
             }
             //发送一次信号
@@ -97,26 +97,23 @@
         dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER);
     }else{
         //IOS6之前
-        addBook =ABAddressBookCreate();
+        addressBookRef =ABAddressBookCreate();
     }
     
-    _addressBookRef = &addBook;
-   
     return tip;
 }
 
 /*  通讯录联系人详细信息 */
 -(NSArray *)AddressBookContactDetails{
     
-    ABAddressBookRef addBook = &_addressBookRef;
+    
     
     NSMutableArray *tempStore = [NSMutableArray array];
     
-
     //获取所有联系人的数组
-    CFArrayRef allLinkPeople = ABAddressBookCopyArrayOfAllPeople(addBook);
+    CFArrayRef allLinkPeople = ABAddressBookCopyArrayOfAllPeople(addressBookRef);
     //获取联系人总数
-    CFIndex number = ABAddressBookGetPersonCount(addBook);
+    CFIndex number = ABAddressBookGetPersonCount(addressBookRef);
     //进行遍历
     for (NSInteger i=0; i<number; i++) {
         
@@ -151,7 +148,7 @@
         }
     }
     
-    CFRelease(addBook);
+    CFRelease(addressBookRef);
     
     NSArray *addressBookModelArray = [NSArray arrayWithArray:tempStore];
     
@@ -161,7 +158,24 @@
 /*  将通信录信息转成json */
 -(NSString *)arrayToJson:(NSArray *)array{
 
-    return nil;
+    NSMutableArray *list = [NSMutableArray array];
+    
+    for (AddressBookPeople *temp in array) {
+        NSLog(@"temp.firstName >> %@",temp.lastName);
+        NSLog(@"temp.phone >> %@",temp.phone);
+        
+        NSDictionary *dic = @{@"name":temp.lastName,@"phone":temp.phone};
+        
+        [list addObject:dic];
+    }
+    
+    
+    NSDictionary *dic = @{@"list":list};
+    
+    NSError *error = nil;
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:dic options:NSJSONWritingPrettyPrinted error:&error];
+    return  [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+
 }
 
 #pragma mark - action
@@ -169,7 +183,9 @@
     //获取通讯录
     NSArray *addressBookModelArray = [self acquisitionSystemAddressBook];
     //转成json
-    [self arrayToJson:addressBookModelArray];
+    NSString *temp = [self arrayToJson:addressBookModelArray];
+    
+    NSLog(@"temp : %@",temp);
 }
 
 
