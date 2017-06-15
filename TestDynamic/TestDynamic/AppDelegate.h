4f2943d1461694868404cbb484b11903a22a4b17
guochao <guochao@zhen-de.com>
2017-06-15 11:18:04 +0800

    第一次尝试 动态化

---
+++ TestDynamic/TestDynamic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDynamic
+//
+//  Created by WangShuChao on 2017/6/15.
+//  Copyright © 2017年 GuoChao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
