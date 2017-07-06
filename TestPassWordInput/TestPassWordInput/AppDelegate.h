a227c9644a05c4564f596c34baca505a340b4699
guochao <guochao@zhen-de.com>
2017-07-06 10:56:13 +0800

    仿支付宝密码输入框 开始

---
+++ TestPassWordInput/TestPassWordInput/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestPassWordInput
+//
+//  Created by WangShuChao on 2017/7/6.
+//  Copyright © 2017年 GuoChao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
