4e3a51ae9172a252764b94df9d9fd58bfc960d8d
guochao <guochao@zhen-de.com>
2017-07-06 11:32:40 +0800

    习惯性存档

---
+++ TestPassWordInput/TestPassWordInput/PassWordField.h
@@ -0,0 +1,15 @@
+//
+//  PassWordField.h
+//  TestPassWordInput
+//
+//  Created by WangShuChao on 2017/7/6.
+//  Copyright © 2017年 GuoChao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PassWordField : UIView
+/*  输入框的个数  默认是6 */
+@property(nonatomic)NSInteger itmeCount;
+
+@end
---
+++ TestPassWordInput/TestPassWordInput/PassWordField.m
@@ -0,0 +1,43 @@
+//
+//  PassWordField.m
+//  TestPassWordInput
+//
+//  Created by WangShuChao on 2017/7/6.
+//  Copyright © 2017年 GuoChao. All rights reserved.
+//
+
+#import "PassWordField.h"
+
+
+@interface PassWordField ()
+
+@property(nonatomic)NSMutableArray *pwdIndicatorArr;
+
+@end
+
+@implementation PassWordField
+
+-(instancetype)init{
+    return [self initWithFrame:CGRectZero];
+}
+
+-(instancetype)initWithFrame:(CGRect)frame{
+    self =  [super initWithFrame:frame];
+    if (self != nil) {
+        [self initUI];
+    }
+    return self;
+}
+
+-(void)initUI{
+    
+    
+    
+
+}
+
+
+
+
+
+@end
